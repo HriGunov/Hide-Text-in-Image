cbc51a4681953642c440ad57151d86ccd67b9ba8
HriGunov <hristo.d.gunov@gmail.com>
2017-04-02 02:18:20 +0300

    Added ImageProcessing class

---
+++ ImgeProcessing.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HideTextInImage
+{
+    class ImgeProcessing
+    {
+       //Takes an image and returns a "black and white" version of it
+      static  Bitmap ToMonoChromatic(Bitmap  imageRef)
+        {
+            double redRatio = 0.30;
+            double greenRatio = 0.59;
+            double blueRatio = 0.11;
+
+            Bitmap image = (Bitmap)imageRef.Clone();
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    Color pixel = image.GetPixel(x, y);
+                    int grayShade = (int)(pixel.R * redRatio + pixel.G * greenRatio + pixel.B * blueRatio);
+                    image.SetPixel(x, y, Color.FromArgb(grayShade, grayShade, grayShade));
+                }
+            }
+
+            return image;
+        } 
+       
+    }
+}
